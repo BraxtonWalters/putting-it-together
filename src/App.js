7eae82df6edc15b51b101be6c879dbdb5f5cb8cd
Braxton Walters <braxton.walters@gmail.com>
2023-08-02 01:33:05 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
+
+const personObj = [
+  {
+    firstName: "Jane", 
+    lastName: "Doe",
+    age: 45,
+    hairColor: "Black"
+  },
+ {
+    firstName: "John", 
+    lastName: "Smith",
+    age: 88,
+    hairColor: "Brown"
+  },
+ {
+    firstName: "Millard", 
+    lastName: "Fillmore",
+    age: 50,
+    hairColor: "Brown"
+  },
+ {
+    firstName: "Maria", 
+    lastName: "Smith",
+    age: 62,
+    hairColor: "Brown"
+  },
+ {
+    firstName: "Homie", 
+    lastName: "Jackel",
+    age: 23,
+    hairColor: "red"
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {personObj.map(key => (
+      <PersonCard props = {key}/>
+      ))}
+      {/* this is the other way kinda of 
+      <PersonCard props = {personObj[0]}/>
+      <PersonCard props = {personObj[1]}/>
+      <PersonCard props = {personObj[2]}/>
+      <PersonCard props = {personObj[3]}/>
+      <PersonCard props = {personObj[4]}/> 
+      */}
     </div>
   );
 }
---
+++ src/components/PersonCard.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react"
+
+const PersonCard = ({props}) => {
+  const [age, setAge] = useState(props.age);
+  //!HAS TO BE "age + 1" BC WE ARE SENDING A !!!VALUE!!! THROUGH !!!STATE!!!
+  const bDayIncrease = () => setAge(age + 1)
+  return (
+    <div>
+        <h1>{props.lastName}, {props.firstName}</h1>
+        <p>Age: {age}</p>
+        <p>Hair Color: {props.hairColor}</p>
+        <button onClick={bDayIncrease}>Birthday Button for {props.firstName} {props.lastName}</button>
+    </div>
+  )
+}
+
+export default PersonCard
+
